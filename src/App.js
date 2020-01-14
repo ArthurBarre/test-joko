8d6af17c658cf7a60393d00f81a33c70f684754a
macarthur <arthurbarre.js@gmail.com>
2020-01-14 14:15:46 +0100

    export app in  git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'
 import './App.css';
+import { parseMolecule } from "./lib/molecule-to-atom";
 
-function App() {
+const MolecularParser = () => {
+  const [ formula, setFormula ] = React.useState( 'Mg3' );
+  const [ atoms, setAtoms ] = React.useState( {} );
+
+  const parseFormula = () => {
+    setAtoms(parseMolecule(formula))
+    console.log(atoms)
+  }
+
+  const printAtoms = Object.entries(atoms).map(([key,value])=>{
+    return (
+      <ul class='atoms'>
+        <li>{key} : {value.toString()}</li>
+      </ul>
+    );
+  })
+  // parseMolecules()
+  return(
+    <div className='molecular-parser'>
+      <div>Enter your chemical formula to parse : </div>
+      <input value={formula} onChange={ event => setFormula( event.target.value ) } type="text"/>
+      <div class='formula'> {formula} </div>
+      <img src={logo} alt="logo" />
+      <button onClick={ () => parseFormula() }>Parse formula</button>
+      { printAtoms }
+    </div>
+  )
+}
+
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MolecularParser/>
     </div>
   );
 }
---
+++ src/lib/molecule-to-atom.js
@@ -0,0 +1,72 @@
+export function molecule(formula) {
+  let _parts = [];
+
+  formula = formula.replace(/[{(]/g, "[").replace(/[})]/g, "]");
+
+  let matches;
+  let stack = [];
+  let regex = /((\[)|(\])|([A-Z][a-z]*))(\d*)/g;
+  while (matches = regex.exec(formula)) {
+      let [_, __, open, close, part, count] = matches;
+      count = parseInt(count) || 1;
+      if (open) {
+          stack.push({
+              formula: "",
+              molecules: []
+          });
+      }
+      else if (close) {
+          let popped = stack.pop();
+          popped.molecules.push(molecule(popped.formula));
+          popped.molecules.forEach((molecule) => {
+              molecule.multiply(count);
+          });
+          if (!stack.length) {
+              popped.molecules.forEach((molecule) => {
+                  _parts = _parts.concat(molecule.parts());
+              });
+          }
+          else {
+              let last = stack[stack.length - 1];
+              last.molecules = last.molecules.concat(popped.molecules);
+          }
+      }
+      else if (stack.length) {
+          stack[stack.length - 1].formula += part + count;
+      }
+      else {
+          _parts.push({
+              part,
+              count
+          });
+      }
+  }
+
+  function multiply(count) {
+      _parts.forEach((part) => {
+          part.count *= count;
+      });
+      return this;
+  }
+
+  function parts() {
+      return _parts;
+  }
+
+  function parse() {
+      return parts().reduce((result, {part, count}) => {
+          result[part] = ~~result[part] + count;
+          return result;
+      }, {});
+  }
+
+  return {
+      parse,
+      parts,
+      multiply
+  };
+}
+
+export function parseMolecule(formula) {
+  return molecule(formula).parse();
+}
